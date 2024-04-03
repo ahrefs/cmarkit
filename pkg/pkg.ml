e35248973a623d259a1cccbe13465685c692d383
Yasunari Watanabe <yasunari.watanabe@ahrefs.com>
2024-04-04 00:07:35 +0800

    use extlib

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,13 +4,15 @@
 open Topkg
 
 let cmdliner = Conf.with_pkg "cmdliner"
+let extlib = Conf.with_pkg "extlib"
 
 let () =
   Pkg.describe "cmarkit" @@ fun c ->
   let cmdliner = Conf.value c cmdliner in
-  let api = ["Cmarkit"; "Cmarkit_renderer";
+  let extlib = Conf.value c extlib in
+  let api = ["Cmarkit_pervasives"; "Cmarkit"; "Cmarkit_renderer";
              "Cmarkit_commonmark"; "Cmarkit_html"; "Cmarkit_latex"]
   in
   Ok [ Pkg.mllib ~api "src/cmarkit.mllib";
        Pkg.doc "doc/index.mld" ~dst:"odoc-pages/index.mld";
-       Pkg.bin ~cond:cmdliner "tool/cmd_main" ~dst:"cmarkit" ]
+       Pkg.bin ~cond:(cmdliner && not extlib) "tool/cmd_main" ~dst:"cmarkit" ]
